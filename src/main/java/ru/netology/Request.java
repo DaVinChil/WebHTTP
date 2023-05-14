43f186856ada5e57b1fcda018b43ed182c5609ef
DaVinCi <fox.winner9@gmail.com>
2023-05-14 14:07:44 +0300

    Add urlencoded post request handling

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -14,10 +14,12 @@ public class Request {
     private HashMap<String, String> headers;
     private String body;
     private List<NameValuePair> queryParams;
+    private List<NameValuePair> postParams;
 
-    private Request(){}
+    private Request() {
+    }
 
-    public boolean hasHeaders(){
+    public boolean hasHeaders() {
         return hasHeaders;
     }
 
@@ -25,31 +27,35 @@ public class Request {
         return path;
     }
 
-    public String getHeader(String header){
-        if(!hasHeaders) {
+    public String getHeader(String header) {
+        if (!hasHeaders) {
             return null;
         }
         return headers.get(header);
     }
 
-    public String getMethod(){
+    public HashMap<String, String> getHeaders(){
+        return headers;
+    }
+
+    public String getMethod() {
         return method;
     }
 
-    public String getBody(){
+    public String getBody() {
         return body;
     }
 
-    public List<NameValuePair> getQueryParams(){
-        return queryParams;
+    public List<NameValuePair> getPostParams() {
+        return postParams;
     }
 
-    public NameValuePair getQueryParam(String name){
-        if(queryParams == null){
+    public NameValuePair getPostParam(String name) {
+        if (postParams == null) {
             return null;
         }
 
-        for(NameValuePair pair : queryParams){
+        for (NameValuePair pair : postParams) {
             if (pair.getName().equals(name)) {
                 return pair;
             }
@@ -58,24 +64,46 @@ public class Request {
         return null;
     }
 
-    public static Builder newBuilder(){
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public NameValuePair getQueryParam(String name) {
+        if (queryParams == null) {
+            return null;
+        }
+
+        for (NameValuePair pair : queryParams) {
+            if (pair.getName().equals(name)) {
+                return pair;
+            }
+        }
+
+        return null;
+    }
+
+    public static Builder newBuilder() {
         return new Request().new Builder();
     }
 
     public class Builder {
+        public static final String URLENCODED = "application/x-www-form-urlencoded";
+        public static final String MULTIPART = "multipart/form-data";
+        public static final String CONTENT_TYPE = "Content-Type";
 
-        private Builder(){}
+        private Builder() {
+        }
 
-        public Builder setMethod(String method){
+        public Builder setMethod(String method) {
             Request.this.method = method;
             return this;
         }
 
-        public String getMethod(){
+        public String getMethod() {
             return Request.this.getMethod();
         }
 
-        public Builder setPath(String path){
+        public Builder setPath(String path) {
             try {
                 URIBuilder uriB = new URIBuilder(path);
                 Request.this.path = uriB.getPath();
@@ -86,41 +114,64 @@ public class Request {
             return this;
         }
 
-        public String getHeaderValue(String header){
+        public String getHeader(String header) {
             return Request.this.getHeader(header);
         }
 
-        public Builder addHeader(String header, String value){
-            if(Request.this.headers == null) {
+        public Builder addHeader(String header, String value) {
+            if (Request.this.headers == null) {
                 Request.this.headers = new HashMap<>();
             }
             Request.this.headers.put(header, value);
+            Request.this.hasHeaders = true;
             return this;
         }
 
-        public Builder addHeaders(List<String> headers){
-            if(Request.this.headers == null) {
+        public Builder addHeaders(List<String> headers) {
+            if (Request.this.headers == null) {
                 Request.this.headers = new HashMap<>();
             }
 
-            for(String headerStr : headers){
+            for (String headerStr : headers) {
                 int deliInd = headerStr.indexOf(':');
                 String header = headerStr.substring(0, deliInd);
-                String value = headerStr.substring(deliInd+1);
+                String value = headerStr.substring(deliInd + 1).trim();
                 Request.this.headers.put(header, value);
+                Request.this.hasHeaders = true;
             }
 
             return this;
         }
 
-        public void setBody(String body){
-            Request.this.body = body;
+        public void parseBodyUrlencoded() {
+            try {
+                URIBuilder urib = new URIBuilder("/?" + body);
+                postParams = urib.getQueryParams();
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        public Request build(){
-            if(Request.this.headers == null) {
+        public void parseBodyMultipart(){
+
+        }
+
+        public void setBody(String body) {
+            Request.this.body = body;
+            String contentType = getHeader(CONTENT_TYPE);
+            if (contentType != null) {
+                if(contentType.equals(URLENCODED)){
+                    parseBodyUrlencoded();
+                } else if(contentType.equals(MULTIPART)){
+                    parseBodyMultipart();
+                }
+            }
+        }
+
+        public Request build() {
+            if (Request.this.headers == null) {
                 Request.this.hasHeaders = false;
-            } else if(Request.this.headers.isEmpty()){
+            } else if (Request.this.headers.isEmpty()) {
                 Request.this.hasHeaders = false;
             } else {
                 Request.this.hasHeaders = true;
